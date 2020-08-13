13ae4742a9224a3e0a2c533678ab5bed7fff4d1a
lixy <li7318009@163.com>
2020-08-13 11:35:41 +0800

    新增学习资源

---
+++ java/thread/CountDownLatchExample.java
@@ -0,0 +1,31 @@
+package thread;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CountDownLatchExample {
+    // 处理文件的数量
+    private static final int threadCount = 6;
+
+    public static void main(String[] args) throws InterruptedException {
+        // 创建一个具有固定线程数量的线程池对象（推荐使用构造方法创建）
+        ExecutorService threadPool = Executors.newFixedThreadPool(10);
+        final CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+        for (int i = 0; i < threadCount; i++) {
+            final int threadnum = i;
+            threadPool.execute(() -> {
+                try {
+                    //处理文件的业务操作
+                } finally {
+                    //表示一个文件已经被完成
+                    countDownLatch.countDown();
+                }
+
+            });
+        }
+        countDownLatch.await();
+        threadPool.shutdown();
+        System.out.println("finish");
+    }
+}
--- java/thread/DeadLockDemo2.java
+++ java/thread/DeadLockDemo2.java
@@ -4,6 +4,13 @@ package thread;
         请求与保持条件 ：一个进程因请求资源而阻塞时，对已获得的资源保持不放。
         不剥夺条件 :线程已获得的资源在末使用完之前不能被其他线程强行剥夺，只有自己使用完毕后才释放资源。
         循环等待条件 :若干进程之间形成一种头尾相接的循环等待资源关系。*/
+
+//引发
+/*面试官 ：那你说说 synchronized 关键字和 volatile 关键字的区别吧！
+        🙋 我 ：synchronized 关键字和 volatile 关键字是两个互补的存在，而不是对立的存在！
+volatile 关键字是线程同步的轻量级实现，所以volatile 性能肯定比 synchronized 关键字要好。但是volatile 关键字只能用于变量而 synchronized 关键字可以修饰方法以及代码块。
+volatile 关键字能保证数据的可见性，但不能保证数据的原子性。synchronized 关键字两者都能保证。
+volatile 关键字主要用于解决变量在多个线程之间的可见性，而 synchronized 关键字解决的是多个线程之间访问资源的同步性。*/
 public class DeadLockDemo2 {
     private static Object resource1 = new Object();//资源 1
     private static Object resource2 = new Object();//资源 2
